e26ad017a45583d36b5857fdf84f926857c977c6
Felipe Oriani <felipeoriani@gmail.com>
2024-07-31 09:10:09 -0300

    Add postgres reset database

--- src/postgres/reset-pg.ts
+++ src/postgres/reset-pg.ts
@@ -1,7 +1,6 @@
 import { pool } from './pg/index.js'
 
-export async function main() {
-  const script = `
+const script = `
 DROP TABLE IF EXISTS "Seat" CASCADE;
 DROP TABLE IF EXISTS "User" CASCADE;
 
@@ -22,22 +21,22 @@ CREATE TABLE "User" (
 
 ALTER TABLE "Seat" ADD CONSTRAINT "FK_User_on_a_Seat" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 `
-  const client = await pool.connect()
+const client = await pool.connect()
 
-  console.log('Resetting database...')
-  await client.query(script)
+console.log('Resetting database...')
+await client.query(script)
 
-  console.log('Inserting data...')
+console.log('Inserting data...')
 
-  await client.query('BEGIN;')
-  for (let i = 1; i <= 1200; i++) {
-    await client.query(`INSERT INTO "User" ("name") VALUES ('User ${i}');`)
-  }
-  for (let i = 1; i <= 1000; i++) {
-    await client.query(`INSERT INTO "Seat" ("version") VALUES (1);`)
-  }
-  await client.query('COMMIT;')
-
-  client.release()
-  console.log('Database reset successfully!')
+await client.query('BEGIN;')
+for (let i = 1; i <= 1200; i++) {
+  await client.query(`INSERT INTO "User" ("name") VALUES ('User ${i}');`)
 }
+for (let i = 1; i <= 1000; i++) {
+  await client.query(`INSERT INTO "Seat" ("version") VALUES (1);`)
+}
+await client.query('COMMIT;')
+
+client.release()
+console.log('Database reset successfully!')
+process.exit(0)
